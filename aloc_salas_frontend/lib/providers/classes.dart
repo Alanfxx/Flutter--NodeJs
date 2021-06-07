d063aeced848d7f615617433bcffede1d41d9e77
Alanfxx <j.alands2@gmail.com>
2021-06-07 14:22:46 -0300

    classes and classrooms implemented

--- aloc_salas_frontend/lib/providers/classes.dart
+++ aloc_salas_frontend/lib/providers/classes.dart
@@ -1,15 +1,20 @@
+import 'dart:convert';
+
 import 'package:aloc_salas_frontend/models/class.dart';
+import 'package:aloc_salas_frontend/utils/constants.dart';
 import 'package:csv/csv.dart';
 import 'package:csv/csv_settings_autodetection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 class Classes with ChangeNotifier {
+  final String _baseUrl = '${Constants.BASE_API_URL}/turmas';
   List<Class> _items = [];
 
-  List<Class> get items {
-    return [..._items];
-  }
+  List<Class> get items => [..._items];
+
+  int get countItems => _items.length;
 
   lerCSV({@required int cenario}) async {
     var d = new FirstOccurrenceSettingsDetector(
@@ -32,7 +37,91 @@ class Classes with ChangeNotifier {
             qualidade: el[7]));
       }
     });
-    _items = classes;
+    await deleteAll([..._items]);
+    _items.clear();
+    await addAll(classes);
+  }
+
+  Future<void> loadClasses() async {
+    final response = await http.get("$_baseUrl");
+    List<dynamic> data = json.decode(response.body);
+
+    _items.clear();
+    if (data != null) {
+      data.forEach((classData) {
+        _items.add(Class(
+            id: classData['id'],
+            disciplina: classData['disciplina'],
+            professor: classData['professor'],
+            dias_horario: classData['dias_horario'],
+            numero_alunos: classData['numero_alunos'],
+            curso: classData['curso'],
+            periodo: classData['periodo'],
+            acessibilidade: classData['acessibilidade'],
+            qualidade: classData['qualidade']));
+      });
+      notifyListeners();
+    }
+    return Future.value();
+  }
+
+  Future<void> addClass(Class newClass) async {
+    final response = await http.post(
+      "$_baseUrl",
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: json.encode({
+        'disciplina': newClass.disciplina,
+        'professor': newClass.professor,
+        'dias_horario': newClass.dias_horario,
+        'numero_alunos': newClass.numero_alunos,
+        'curso': newClass.curso,
+        'periodo': newClass.periodo,
+        'acessibilidade': newClass.acessibilidade,
+        'qualidade': newClass.qualidade
+      }),
+    );
+    print(response.body);
+    _items.add(Class(
+        id: response.body,
+        disciplina: newClass.disciplina,
+        professor: newClass.professor,
+        dias_horario: newClass.dias_horario,
+        numero_alunos: newClass.numero_alunos,
+        curso: newClass.curso,
+        periodo: newClass.periodo,
+        acessibilidade: newClass.acessibilidade,
+        qualidade: newClass.qualidade));
     notifyListeners();
+  }
+
+  Future<void> deleteClass(String id) async {
+    final index = _items.indexWhere((class1) => class1.id == id);
+    if (index >= 0) {
+      final class1 = _items[index];
+      _items.remove(class1);
+      notifyListeners();
+
+      final response = await http.delete("$_baseUrl/${class1.id}");
+
+      if (response.statusCode >= 400) {
+        _items.insert(index, class1);
+        notifyListeners();
+      }
+    }
+  }
+
+  Future<void> addAll(List<Class> items) async {
+    items.forEach((class1) async {
+      await addClass(class1);
+      // print(classroom.id_sala);
+    });
+  }
+
+  Future<void> deleteAll(List<Class> items) async {
+    items.forEach((class1) async {
+      await deleteClass(class1.id);
+    });
   }
 }
--- aloc_salas_frontend/lib/providers/classrooms.dart
+++ aloc_salas_frontend/lib/providers/classrooms.dart
@@ -1,15 +1,20 @@
+import 'dart:convert';
+
 import 'package:aloc_salas_frontend/models/classroom.dart';
+import 'package:aloc_salas_frontend/utils/constants.dart';
 import 'package:csv/csv.dart';
 import 'package:csv/csv_settings_autodetection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 class Classrooms with ChangeNotifier {
+  final String _baseUrl = '${Constants.BASE_API_URL}/salas';
   List<Classroom> _items = [];
 
-  List<Classroom> get items {
-    return [..._items];
-  }
+  List<Classroom> get items => [..._items];
+
+  int get countItems => _items.length;
 
   lerCSV({@required int cenario}) async {
     var d = new FirstOccurrenceSettingsDetector(
@@ -28,7 +33,100 @@ class Classrooms with ChangeNotifier {
             qualidade: el[3]));
       }
     });
-    _items = classrooms;
+    await addAll(classrooms);
+  }
+
+  Future<void> loadClassrooms() async {
+    final response = await http.get("$_baseUrl");
+    List<dynamic> data = json.decode(response.body);
+
+    _items.clear();
+    if (data != null) {
+      data.forEach((classroomData) {
+        _items.add(Classroom(
+            id: classroomData['id'],
+            id_sala: classroomData['id_sala'],
+            numero_cadeiras: classroomData['numero_cadeiras'],
+            acessivel: classroomData['acessivel'],
+            qualidade: classroomData['qualidade']));
+      });
+      notifyListeners();
+    }
+    return Future.value();
+  }
+
+  Future<void> addClassroom(Classroom newClassroom) async {
+    final response = await http.post(
+      "$_baseUrl",
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: json.encode({
+        'id_sala': newClassroom.id_sala,
+        'numero_cadeiras': newClassroom.numero_cadeiras,
+        'acessivel': newClassroom.acessivel,
+        'qualidade': newClassroom.qualidade
+      }),
+    );
+    print(response.body);
+    _items.add(Classroom(
+        id: response.body,
+        id_sala: newClassroom.id_sala,
+        numero_cadeiras: newClassroom.numero_cadeiras,
+        acessivel: newClassroom.acessivel,
+        qualidade: newClassroom.qualidade));
     notifyListeners();
   }
+
+  Future<void> deleteClassroom(String id) async {
+    final index = _items.indexWhere((classr) => classr.id == id);
+    if (index >= 0) {
+      final classroom = _items[index];
+      _items.remove(classroom);
+      notifyListeners();
+
+      final response = await http.delete("$_baseUrl/${classroom.id}");
+
+      if (response.statusCode >= 400) {
+        _items.insert(index, classroom);
+        notifyListeners();
+      }
+    }
+  }
+
+  Future<void> addAll(List<Classroom> items) async {
+    items.forEach((classroom) async {
+      await addClassroom(classroom);
+      // print(classroom.id_sala);
+    });
+  }
+
+  Future<void> deleteAll(items) async {
+    items.forEach((classroom) async {
+      await deleteClassroom(classroom.id);
+    });
+  }
 }
+  // Future<void> updateClassroom(Classroom classroom) async {
+  //   if (classroom == null || classroom.id == null) {
+  //     return;
+  //   }
+
+  //   final index = _items.indexWhere((classr) => classr.id == classroom.id);
+  //   if (index >= 0) {
+  //     await http.put(
+  //       "$_baseUrl/${classroom.id}",
+  //       headers: <String, String>{
+  //         'Content-Type': 'application/json; charset=UTF-8',
+  //       },
+  //       body: json.encode({
+  //         'id_sala': classroom.id_sala,
+  //         'numero_cadeiras': classroom.numero_cadeiras,
+  //         'acessivel': classroom.acessivel,
+  //         'qualidade': classroom.qualidade
+  //       }),
+  //     );
+  //     _items[index] = classroom;
+  //     notifyListeners();
+  //   }
+  // }
